da473c772579c4fa8e0c3cfc00efdb713a883836
xirurgx3d <xirurgx3d@gmail.com>
2021-06-29 18:02:17 +0300

    mov app

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -1,20 +1,20 @@
 import React, { useEffect, useState } from 'react'; 
-import Main from './components/Dashboard';
-import { checkUser } from './api/Api_user';
+import Main from '../components/Dashboard';
+import { checkUser } from '../api/Api_user';
 import { useHistory } from 'react-router-dom';
-import Loader from './components/loader';
+import Loader from '../components/loader';
 import {observer} from "mobx-react";
-import UserStore from './store/UserStore';
+import UserStore from '../store/UserStore';
 
 import { AppBar, Badge, Box, Container, CssBaseline, Divider, Drawer, Grid, IconButton, Link, List, ListItem, ListItemIcon, ListItemText, Paper, Toolbar, Typography } from '@material-ui/core';
 import DashboardIcon from '@material-ui/icons/Dashboard';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import clsx from 'clsx';
-import { useStyles } from './styled/material_styles';
+import { useStyles } from '../styled/material_styles';
 import { Copyright } from '@material-ui/icons';
-import Router from './routes/Router';
-import { RouteEnv } from './constants/constRouter';
+import Router from '../routes/Router';
+import { RouteEnv } from '../constants/constRouter';
 
 
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App/App';
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import store from './redux/store';
