72b849d7df50470a50888f600ec5b62a1be08563
xirurgx3d <xirurgx3d@gmail.com>
2021-08-16 13:24:16 +0300

    build

---
+++ frontend/build/precache-manifest.5f0502c29f558fe6295049993755f594.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eb75ab67dd88fee47e6d4a5e3febb52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ebe613550659f427e2c",
+    "url": "/static/css/main.533fb9ef.chunk.css"
+  },
+  {
+    "revision": "2eced36b0fea3b1a0592",
+    "url": "/static/js/2.9a1d9a08.chunk.js"
+  },
+  {
+    "revision": "e1c63bb801230b4e5a8d372e80705fb2",
+    "url": "/static/js/2.9a1d9a08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ebe613550659f427e2c",
+    "url": "/static/js/main.84f43ca7.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
