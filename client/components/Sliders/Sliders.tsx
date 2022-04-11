05ace2d0b76f369e8a6d8e1430409f0b76bfe1b2
Alex K <akuzin20@gmail.com>
2022-04-11 17:19:19 +0300

    banners update

--- client/components/Sliders/Sliders.tsx
+++ client/components/Sliders/Sliders.tsx
@@ -1,5 +1,5 @@
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import { useState } from 'react';
 import "slick-carousel/slick/slick-theme.css";
 
@@ -16,11 +16,11 @@ const MainSlider = (): JSX.Element => {
 
   return (
     <>
-      <section className="top-slider">
+      <section className="top-slider main-slider">
         <Slider className="slide headslide" ref={slide => setNav1(slide)} {...settings}>
           <img className="slide__image" src="/assets/img/main-banner/1.jpg" alt="slide-1" />
           <img className="slide__image" src="/assets/img/main-banner/2.jpg" alt="slide-1" />
-          <img className="slide__image" src="/assets/img/main-banner/3.jpg" alt="slide-1" />
+          <img className="slide__image" src="/assets/img/main-banner/3.png" alt="slide-1" />
           <img className="slide__image" src="/assets/img/main-banner/4.png" alt="slide-1" />
           <img className="slide__image" src="/assets/img/main-banner/5.jpg" alt="slide-1" />
         </Slider>
