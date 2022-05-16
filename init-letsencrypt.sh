e0b0a5777fc11745176dbea4f91f90e81af803e1
xirurgx3d <xirurgx3d@gmail.com>
2022-05-16 13:23:18 +0300

    ssl

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(xn--80ajcndddqbgqne2gxc.xn--p1ai,www.xn--80ajcndddqbgqne2gxc.xn--p1ai)
+domains=(xn--80ajcndddqbgqne2gxc.xn--p1ai)
 rsa_key_size=4096
 data_path="./certbot"
 email="" # Adding a valid address is strongly recommended
