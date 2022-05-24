ad42e1639dd719b06ebb3733d34e077f162e537e
xirurgx3d <xirurgx3d@gmail.com>
2022-05-24 16:43:22 +0300

    Update AboutSlide.tsx

--- client/components/AboutSlide/AboutSlide.tsx
+++ client/components/AboutSlide/AboutSlide.tsx
@@ -3,8 +3,8 @@ import "slick-carousel/slick/slick.css";
 import {SetStateAction, useState} from 'react';
 import "slick-carousel/slick/slick-theme.css";
 
-const AboutSlider = () => {
-  const [nav, setNav] = useState<any>();
+const AboutSlider = (): JSX.Element => {
+  const [nav, setNav] = useState<SetStateAction<any>>();
 
   const settings = {
     dots: true,
@@ -18,7 +18,16 @@ const AboutSlider = () => {
   return (
     <>
       <section className="top-slider allstile aboutslide">
-      
+      <Slider className="slide  aboutslide" ref={slide => setNav(slide)} {...settings}>
+          <img src="/assets/img/about-slider/img1.jpg" alt="middle slider" />
+          <img src="/assets/img/about-slider/img2.jpg" alt="middle slider" />
+          <img src="/assets/img/about-slider/img3.jpg" alt="middle slider" />
+          <img src="/assets/img/about-slider/img4.jpg" alt="middle slider" />
+          <img src="/assets/img/about-slider/img5.jpg" alt="middle slider" />
+          <img src="/assets/img/about-slider/img6.jpg" alt="middle slider" />
+          <img src="/assets/img/about-slider/img7.jpg" alt="middle slider" />
+          <img src="/assets/img/about-slider/img8.jpg" alt="middle slider" />
+        </Slider>
       </section>
     </>
   )
