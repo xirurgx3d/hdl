1276e3f22a106bbd7d560a28fee55f4dd82bd429
xirurgx3d <xirurgx3d@gmail.com>
2022-05-25 16:21:23 +0300

    Update galleryHead.tsx

--- frontend/src/components/sliders/galleryHead/galleryHead.tsx
+++ frontend/src/components/sliders/galleryHead/galleryHead.tsx
@@ -4,7 +4,7 @@ import { Button, Container, Grid, Paper, Typography } from '@material-ui/core';
 import clsx from 'clsx';
 import { useStyles } from '../../../styled/material_styles';
 import { RouteEnv } from '../../../constants/constRouter';
-import GalleryHeadList from './GalleryHeadList';
+
 
 const GalleryHead: React.FC = (): JSX.Element => {
   const classes = useStyles()
@@ -20,7 +20,7 @@ const GalleryHead: React.FC = (): JSX.Element => {
           <Button className={classes.add_buttons} variant="contained" color="primary">
             <Link to={RouteEnv.SLIDERS_GALLERYHEAD} >Добавить Слайдер</Link>
           </Button>
-          <GalleryHeadList />  
+
             
         </Paper>
       </Grid>
