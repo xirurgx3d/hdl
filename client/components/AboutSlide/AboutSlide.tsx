1600917d9817ce80a3c4ac2136ee2b9b94dca8be
xirurgx3d <xirurgx3d@gmail.com>
2022-05-25 15:32:06 +0300

    Update AboutSlide.tsx

--- client/components/AboutSlide/AboutSlide.tsx
+++ client/components/AboutSlide/AboutSlide.tsx
@@ -17,7 +17,7 @@ const AboutSlider = () => {
 
   return (
       <section className="top-slider allstile aboutslide">
-      <Slider>
+      <Slider ref={slide => setNav(slide)} {...settings}>
           <img src="/assets/img/about-slider/img1.jpg" alt="middle slider" />
           <img src="/assets/img/about-slider/img2.jpg" alt="middle slider" />
           <img src="/assets/img/about-slider/img3.jpg" alt="middle slider" />
