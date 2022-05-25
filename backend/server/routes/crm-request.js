20cea4c9c6954f4eb1e2dee6e67ca00d15cd0350
xirurgx3d <xirurgx3d@gmail.com>
2022-05-25 17:41:05 +0300

    Update crm-request.js

--- backend/server/routes/crm-request.js
+++ backend/server/routes/crm-request.js
@@ -6,7 +6,7 @@ const router = express.Router()
 const API_KEY = '243_repfjrqAkdXjFjRSRDHRCxNpVtWrxDc6NYxcMJaGhcvnQaMY';
 
 // for domoplaner
-router.post('/send',  function(req, res) {
+router.post('/send', async  function(req, res) {
 	/**/
 		console.log(req.body);
     request.post({
@@ -24,7 +24,48 @@ router.post('/send',  function(req, res) {
 				!err ? res.status(200).json('ok') : res.status(500).json('no')
     });
 		
+		try {
+
 		
+			let transporter = nodemailer.createTransport({
+				host: 'smtp.beget.com',
+				port: 465,
+				secure: true,
+				auth: {
+						user: 'solar@arcadacrimea.ru',
+						pass: '*Dbln4rk'
+				}
+			});
+			/*
+			let testEmailAccount = await nodemailer.createTestAccount()
+	
+			let transporter = nodemailer.createTransport({
+				host: 'smtp.ethereal.email',
+				port: 587,
+				secure: false,
+				auth: {
+					user: testEmailAccount.user,
+					pass: testEmailAccount.pass,
+				},
+			})*/
+		
+			let info = await transporter.sendMail({
+				from: 'solar@arcadacrimea.ru', // sender address
+				to: "bambinix3dx@yandex.ru", // list of receivers
+				subject: "Hello ✔", // Subject line
+				text: "Hello world?", // plain text body
+				html: "<b>Hello world?</b>", // html body
+				
+			});
+	
+			console.log("Message sent: %s", info.messageId);
+		// Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+	
+		// Preview only available when sending through an Ethereal account
+		console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+		} catch (error) {
+			console.log(error);
+		}	
 });
 export default router
 
